a13186aa95083071627ab991475ecc5538282617
Gustavo.Silva <Gustavodasilva2003@outlook.com>
2024-10-30 11:45:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -836,7 +836,7 @@ function calculateCurrentAttributes() {
   
   }
 
-  attributes.vida += (forLevel * raceData[race].vidaPorNivel.for) + (desLevel * raceData[race].vidaPorNivel.des) + (conLevel * raceData[race].vidaPorNivel.con) -137;
+  attributes.vida += (forLevel * raceData[race].vidaPorNivel.for) + (desLevel * raceData[race].vidaPorNivel.des) + (conLevel * raceData[race].vidaPorNivel.con) -110;
   attributes.sanidade += (intLevel * raceData[race].sanidadePorNivel.int) + (sabLevel * raceData[race].sanidadePorNivel.sab) + (carLevel * raceData[race].sanidadePorNivel.car)-30;
   attributes.especial += level * raceData[race].especialPorNivel; 
   let valorSubtrair = 0; // Inicializa o valor de subtração
